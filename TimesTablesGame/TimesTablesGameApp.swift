8d3eef1dd2ad7368c2c057cfa366654dfa009b5e
Omolemo Mashigo <omomashigo@gmail.com>
2025-02-26 23:31:25 +0200

    Initial Commit

---
+++ TimesTablesGame/TimesTablesGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesGameApp.swift
+//  TimesTablesGame
+//
+//  Created by Omolemo Mashigo on 2025/02/26.
+//
+
+import SwiftUI
+
+@main
+struct TimesTablesGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimesTablesGameTests/TimesTablesGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesGameTests.swift
+//  TimesTablesGameTests
+//
+//  Created by Omolemo Mashigo on 2025/02/26.
+//
+
+import Testing
+@testable import TimesTablesGame
+
+struct TimesTablesGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
